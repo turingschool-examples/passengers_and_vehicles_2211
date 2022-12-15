794fbaf6c6404398beb544a341e83d1cdfe7f56f
Meredith Trevino <meredithtrevino@gmail.com>
2022-12-15 09:14:08 -0700

    adds working name and age test

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -4,6 +4,15 @@ RSpec.describe Passenger do
 
     it 'exists' do
         charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+        taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
+
         expect(charlie).to be_an_instance_of(Passenger)
+    end
+    
+    it 'has a name and age' do
+        charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+
+        expect(charlie.name).to eq("Charlie")
+        expect(charlie.age).to eq(18)
     end
 end
