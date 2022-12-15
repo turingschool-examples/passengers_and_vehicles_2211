8e3fb2c1902f453fbb5d7fa1dece4d04f6822a80
Weston Sandfort <sandfortw@gmail.com>
2022-12-15 09:33:50 -0700

    Add a vehicle class to pass vehicle spec

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -18,14 +18,14 @@ describe Vehicle do
     expect(vehicle.model).to eq("Civic")
   end
 
-  xit 'can be speeding and not speeding' do
+  it 'can be speeding and not speeding' do
     vehicle = Vehicle.new("2001", "Honda", "Civic")  
     expect(vehicle.speeding?).to be(false)
     vehicle.speed
     expect(vehicle.speeding?).to be(true)
   end
 
-  xit 'can have passengers' do
+  it 'can have passengers' do
     vehicle = Vehicle.new("2001", "Honda", "Civic")  
     expect(vehicle.passengers).to eq([])
     charlie = Passenger.new({"name" => "Charlie", "age" => 18})
@@ -37,8 +37,14 @@ describe Vehicle do
     expect(vehicle.passengers).to eq([charlie, jude, taylor])
   end
 
-  xit 'can count adults' do
+  it 'can count adults' do
     vehicle = Vehicle.new("2001", "Honda", "Civic")
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    jude = Passenger.new({"name" => "Jude", "age" => 20})  
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})  
+    vehicle.add_passenger(charlie) 
+    vehicle.add_passenger(jude)  
+    vehicle.add_passenger(taylor)    
     expect(vehicle.num_adults).to eq(2)
   end
 
