534c5ceeaf1230580e22802814ae9e9956585f6e
Matt Enyeart <menyeart1@gmail.com>
2022-12-15 09:16:34 -0700

    Add the adult? method and pass the adult test

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,11 +1,16 @@
 class Passenger
 
-attr_reader :name, :age
+  attr_reader :name, :age
 
   def initialize(attributes)
     @name = attributes["name"]
     @age = attributes["age"]
-
   end
+
+  def adult?
+    @age >= 18
+  end
+
+
 
 end
