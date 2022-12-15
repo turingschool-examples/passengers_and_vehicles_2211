e1a228c16f4037e1596192dc02a6c38b7121fcc1
Bobby Luly <bobbyluly@gmail.com>
2022-12-15 09:51:17 -0700

    Test- add test 4

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -4,28 +4,29 @@ RSpec.describe Passenger do
   let(:charlie) { Passenger.new({"name" => "Charlie", "age" => 18}) }
   let(:taylor) { Passenger.new({"name" => "Taylor", "age" => 12}) }
 
-  
-  it "exists" do
-    expect(charlie).to be_an_instance_of(Passenger)
-    expect(taylor).to be_an_instance_of(Passenger)
-  end
+  describe 'Iteration 1' do
+    it "exists" do
+      expect(charlie).to be_an_instance_of(Passenger)
+      expect(taylor).to be_an_instance_of(Passenger)
+    end
 
-  it "has a passenger name and age" do
-    expect(charlie.name).to eq("Charlie")
-    expect(charlie.age).to eq(18)
-  end
+    it "has a passenger name and age" do
+      expect(charlie.name).to eq("Charlie")
+      expect(charlie.age).to eq(18)
+    end
 
-  it "can tell if a passenger is an adult" do
-    expect(charlie.adult?).to be true
-    expect(taylor.adult?).to be false
-  end
+    it "can tell if a passenger is an adult" do
+      expect(charlie.adult?).to be true
+      expect(taylor.adult?).to be false
+    end
 
-  it "can tell who the driver is" do
-    expect(charlie.driver?).to be false
+    it "can tell who the driver is" do
+      expect(charlie.driver?).to be false
 
-    charlie.drive
+      charlie.drive
 
-    expect(charlie.driver?).to be true
+      expect(charlie.driver?).to be true
+    end
   end
 
 end
