d67d31ea1c7aad85c8759885e0ec05856cb031f8
Melony Franchini <meltravelz@gmail.com>
2022-12-15 09:21:14 -0700

    test: Create initialize test & method

---
+++ spec/passenger_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require './lib/passenger'
+
+RSpec.describe Passenger do
+    let(:charlie) { Passenger.new({"name" => "Charlie", "age" => 18}) }  
+    let(:taylor) { Passenger.new({"name" => "Taylor", "age" => 12}) }
+
+    it "exists" do
+        expect(charlie).to be_instance_of(Passenger)
+        expect(taylor).to be_instance_of(Passenger)
+    end
+
+
+
+end
