333a23fc539c703b7d801dfef7a753c91675e846
KaraJH <karas.jones@yahoo.com>
2022-12-15 10:29:51 -0700

    test: Helper method format revenue

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -119,4 +119,34 @@ RSpec.describe Park do
       expect(park.calculate_revenue).to eq("60.0")
     end
   end
+
+  describe '#format_revenue' do 
+    it 'formats the revenue with trailing zero and dollar sign' do 
+        park = Park.new("Zion National Park", 15.00) 
+
+      vehicle_1 = Vehicle.new("2001", "Honda", "Civic") 
+      vehicle_2 = Vehicle.new("2005", "Chevrolet", "Aveo")  
+
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18}) 
+      jude = Passenger.new({"name" => "Jude", "age" => 20}) 
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+      cornelius = Passenger.new({"name" => "Cornelius", "age" => 50}) 
+      maud = Passenger.new({"name" => "Maud", "age" => 50}) 
+      beatrice = Passenger.new({"name" => "Beatrice", "age" => 10}) 
+
+      vehicle_1.add_passenger(charlie)  
+      vehicle_1.add_passenger(jude) 
+      vehicle_1.add_passenger(taylor) 
+
+      vehicle_2.add_passenger(cornelius)  
+      vehicle_2.add_passenger(maud) 
+      vehicle_2.add_passenger(beatrice) 
+
+      park.add_vehicle(vehicle_1)
+      park.add_vehicle(vehicle_2)
+
+      park.calculate_revenue
+      expect(park.format_revenue).to eq("$60.00")
+    end
+  end
 end
