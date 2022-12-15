e1c0100b336b02356015d02b07e4518a4b59b80f
Brian Hayes <Brian.hayes1200@gmail.com>
2022-12-15 10:24:22 -0700

    Test: add tests for adults and minors in park for class national park

--- spec/national_park_spec.rb
+++ spec/national_park_spec.rb
@@ -91,4 +91,30 @@ RSpec.describe NationalPark do
 
     expect(park.attendees).to eq(["Brian", "Carolyn", "Charlie", "John", "Jude", "Taylor"])
   end
+
+  it 'will find all of the adults and children in the park' do 
+    park = NationalPark.new("Grand Canyon", 30)
+    vehicle1 = Vehicle.new("2001", "Honda", "Civic")
+    vehicle2 = Vehicle.new("2019", "Toyota", "4Runner")
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})  
+    jude = Passenger.new({"name" => "Jude", "age" => 20}) 
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+    brian = Passenger.new({"name" => "Brian", "age" => 20})
+    john = Passenger.new({"name" => "John", "age" => 18})
+    carolyn = Passenger.new({"name" => "Carolyn", "age" => 23})
+
+    vehicle1.add_passenger(charlie)
+    vehicle1.add_passenger(jude)    
+    vehicle1.add_passenger(taylor)  
+
+    vehicle2.add_passenger(brian)
+    vehicle2.add_passenger(john)
+    vehicle2.add_passenger(carolyn)
+
+    park.add_vehicle(vehicle1)
+    park.add_vehicle(vehicle2)
+
+    expect(park.adults).to eq(["Brian", "Carolyn", "Charlie", "John", "Jude"])
+    expect(park.minors).to eq(["Taylor"])
+  end
 end
