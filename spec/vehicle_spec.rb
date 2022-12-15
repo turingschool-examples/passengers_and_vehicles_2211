cfa6a73d3cc03d2cdc638b8ac82209d991ec6fba
Matt Enyeart <menyeart1@gmail.com>
2022-12-15 09:33:12 -0700

    Add 3 tests for vehicle instance variables, make, model, year

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -2,6 +2,26 @@ require './lib/passenger.rb'
 require './lib/vehicle.rb'
 
 describe Vehicle do
+  
+it "exists" do
+  vehicle = Vehicle.new("2001", "Honda", "Civic")
+  expect(vehicle).to be_an_instance_of(Vehicle)
+end
+
+it "has a year" do    
+  vehicle = Vehicle.new("2001", "Honda", "Civic")
+  expect(vehicle.year).to eq("2001")
+end
+
+it "has a make" do    
+  vehicle = Vehicle.new("2001", "Honda", "Civic")
+  expect(vehicle.make).to eq("Honda")
+end
+
+it "has a model" do    
+  vehicle = Vehicle.new("2001", "Honda", "Civic")
+  expect(vehicle.model).to eq("Civic")
+end
 
 
 
