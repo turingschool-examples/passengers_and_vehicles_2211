693fc030ccd7f061498dfbf247d6aaa96e85c58a
Harrison Ryan <hwryan12@gmail.com>
2022-12-15 11:57:44 -0600

    docs: Pass first 4 It3 test

--- lib/park.rb
+++ lib/park.rb
@@ -1,8 +1,23 @@
 class Park
-  attr_reader :name, :price
+  attr_reader :name, 
+              :price,
+              :vehicles
 
   def initialize(name, price)
     @name = name
     @price = price
+    @vehicles = []
+  end
+
+  def add_vehicle(vehicle)
+    @vehicles << vehicle
+  end
+
+  def list_all_passengers
+    @vehicles.map do |vehicle|
+      vehicle.passengers.select do |passenger|
+        passenger
+      end
+    end.flatten
   end
 end
--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -5,6 +5,14 @@ require './lib/park'
 
 RSpec.describe Park do
   let(:park) { Park.new("Denali", 30) }
+  let(:honda) { Vehicle.new("2008", "Honda", "Civic") }
+  let(:toyata) { Vehicle.new("1999", "Toyata", "4Runner") }
+  let(:ford) { Vehicle.new("2017", "Ford", "F150") }
+  let(:taylor) { Passenger.new({name: "Taylor", age: 12}) }
+  let(:charlie) { Passenger.new({name: "Charlie", age: 18}) }
+  let(:jude) { Passenger.new({name: "Jude", age: 20}) }
+  let(:kutter) { Passenger.new({name: "Kutter", age: 26}) }
+  
   it 'exists' do
 
     expect(park).to be_an_instance_of(Park)
@@ -14,5 +22,27 @@ RSpec.describe Park do
 
     expect(park.name).to eq("Denali")
     expect(park.price).to eq(30)
+  end
+
+  it 'can list all vehicles in the park' do
+    
+    park.add_vehicle(honda)
+    park.add_vehicle(toyata)
+    park.add_vehicle(ford)
+
+    expect(park.vehicles).to eq([honda, toyata, ford])
+  end
+
+  it 'can list all passengers in the park' do
+    honda.add_passenger(charlie)
+    honda.add_passenger(taylor)
+    toyata.add_passenger(jude)
+    ford.add_passenger(kutter)
+
+    park.add_vehicle(honda)
+    park.add_vehicle(toyata)
+    park.add_vehicle(ford)
+
+    expect(park.list_all_passengers).to eq([charlie, taylor, jude, kutter])
   end
 end
