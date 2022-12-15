6a41bb799f482cad85c49de0d1cbfed18f531938
KaraJH <karas.jones@yahoo.com>
2022-12-15 09:21:38 -0700

    feat: Passengers array added to vehicle class

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,12 +1,13 @@
 class Vehicle 
 
-  attr_reader :year, :make, :model 
+  attr_reader :year, :make, :model, :passengers
 
   def initialize(year, make, model)
     @year = year
     @make = make
     @model = model
     @is_speeding = false
+    @passengers = []
   end
 
   def speeding? 
