fa98f763104031043c3e534c951d918b76ec7348
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-12-15 08:17:21 -0800

    Test: create test for adult? method

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -4,11 +4,19 @@ describe Passenger do
   let(:charlie) {Passenger.new({'name' => 'Charlie', 'age' => 18})}
   let(:taylor) {Passenger.new({'name' => 'Taylor', 'age' => 12})}
 
-  it 'has attributes' do
+  it 'exists' do
     expect(charlie).to be_a(Passenger)
     expect(taylor).to be_a(Passenger)
-
+  end
+    
+  it'has attributes' do
     expect(charlie.name).to eq('Charlie')
     expect(charlie.age).to eq (18)
-  end 
+  end
+
+  it 'can determine if the passenger is an adult' do
+    expect(charlie.adult?).to be true
+    expect(taylor.adult?).to be false
+  end
 end
+ 
