f04a86f923a20f94306ff8aede525a39d949cd7d
Matt Enyeart <menyeart1@gmail.com>
2022-12-15 09:08:13 -0700

    Create a test for name

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -6,4 +6,9 @@ it "exsits" do
   passenger = Passenger.new()  
 end
 
+it "can have a name" do
+  charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+  expect(charlie.name).to eq("Chalie")
+end
+
 end
