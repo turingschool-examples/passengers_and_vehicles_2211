baec51eeb4efb5199fa4ced896fc9d94d4c1d4f1
Melony Franchini <meltravelz@gmail.com>
2022-12-15 09:37:40 -0700

    test: Pass it exists test & create initialize method

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,7 +1,12 @@
 class Vehicle
+    attr_reader :year, :brand, :model
+
+    def initialize(year, brand, model)
+        @year = year
+        @brand = brand
+        @model = model
+    end
 
 
 
-
-    
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Vehicle do
     let(:vehicle) { Vehicle.new("2001", "Honda", "Civic") }
 
     it "exists" do
-        
+        expect(vehicle).to be_instance_of(Vehicle)
     end
 
 
