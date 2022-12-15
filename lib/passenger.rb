38d2be2f3865425a69d3b16c39f0b287e7dccf4e
Harrison Ryan <hwryan12@gmail.com>
2022-12-15 10:29:22 -0600

    docs: Complete  It1

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -3,5 +3,24 @@ class Passenger
   def initialize(info)
     @name = info[:name]
     @age = info[:age]
+    @adult = true
+    @driver = false
+  end
+
+  def adult?
+    if @age >= 18
+      @adult
+    else
+      @adult = false 
+    end
+  end
+
+  def driver?
+    @driver
+  end
+
+  def drive
+  @driver = true
   end
 end
+
--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -18,4 +18,23 @@ RSpec.describe Passenger do
     expect(@charlie.name).to eq('Charlie')
     expect(@charlie.age).to eq(18)
   end
+
+  it "can tell if an adult" do
+    @charlie = Passenger.new({name: "Charlie", age: 18})
+    @taylor = Passenger.new({name: "Taylor", age: 12}) 
+
+    expect(@charlie.adult?).to be true
+    expect(@taylor.adult?).to be false
+  end
+
+  it "can show who can drive" do
+    @charlie = Passenger.new({name: "Charlie", age: 18})
+    @taylor = Passenger.new({name: "Taylor", age: 12}) 
+
+    expect(@charlie.driver?).to be false
+    
+    @charlie.drive
+
+    expect(@charlie.driver?).to be true
+  end
 end
