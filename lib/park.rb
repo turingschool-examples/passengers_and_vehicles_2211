97bfce84c6d16e068bffc1278663b86ed5157a12
KaraJH <karas.jones@yahoo.com>
2022-12-15 11:00:50 -0700

    feat: Patrons hash built with helper methods

--- lib/park.rb
+++ lib/park.rb
@@ -35,4 +35,35 @@ class Park
     format.reverse.concat("$").reverse
   end
 
+  def patron_list 
+    patrons_by_name = {}
+
+    patrons_by_name[:all_attendees] = visitor_names
+    patrons_by_name[:adults] = adult_visitors
+    patrons_by_name[:minors] = minor_visitors
+   
+    patrons_by_name
+  end
+
+  def visitor_names 
+    visitors.map do |visitor|
+      visitor.name
+    end.sort
+  end
+
+  def adult_visitors 
+    visitors.map do |visitor|
+      if visitor.adult? == true 
+        visitor.name
+      end
+    end.compact.sort
+  end
+
+  def minor_visitors 
+    visitors.map do |visitor| 
+      if visitor.adult? == false 
+        visitor.name
+      end
+    end.compact.sort
+  end
 end
