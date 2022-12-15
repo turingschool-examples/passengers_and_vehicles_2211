c5adf010eb045f1e9ce7b1323a93290af14bc710
Axel De La Guardia <axeldelaguardia@gmail.com>
2022-12-15 09:49:12 -0700

    Add: #admit_vehicle method and list vehicles entered

--- lib/park.rb
+++ lib/park.rb
@@ -1,12 +1,16 @@
 class Park
 	attr_reader :name,
-							:admission_price
+							:admission_price,
+							:vehicles_entered
 
 	def initialize(park_info)
 		@name = park_info[:name]
 		@admission_price = park_info[:admission_price]
+		@vehicles_entered = []
 	end
 
 
-	
+	def admit_vehicle(vehicle)
+		@vehicles_entered << vehicle
+	end
 end
--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -8,18 +8,25 @@ describe Park do
   let(:jude) {Passenger.new({"name" => "Jude", "age" => 20})}
   let(:taylor) {Passenger.new({"name" => "Taylor", "age" => 12})}
   let(:vehicle) {Vehicle.new("2001", "Honda", "Civic")}
-	let(:park) {Park.new({name: "Olympia National Park", admission_price: 10})}
+	let(:park) {Park.new({name: "Olympic National Park", admission_price: 10})}
 
 	describe 'initialize' do
 
 		it 'exists and has attributes' do
 
       expect(park).to be_a(Park)
-      expect(park.name).to eq("Olympia National Park")
+      expect(park.name).to eq("Olympic National Park")
       expect(park.admission_price).to eq(10)
 		end
 
+		it 'can list all vehicles that enter the park' do
 
+			expect(park.vehicles_entered).to eq([])
+			
+			park.admit_vehicle(vehicle)
+
+			expect(park.vehicles_entered).to eq([vehicle])
+		end
 
 
 	end
