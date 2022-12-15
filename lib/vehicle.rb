301fa01d3eacd9804642b3983310250b9cbb02d5
Hady Matar <hadyematar@gmail.com>
2022-12-15 09:37:30 -0700

    Complete iteration 2

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,13 +1,42 @@
 class Vehicle
       attr_reader :year, 
-                  :brand, 
-                  :model
+                  :make, 
+                  :model, 
+                  :passengers
 
-  def initialize(year, brand, model)
+  def initialize(year, make, model)
     
     @year = year
-    @brand = brand
+    @make = make
     @model = model 
+    @speeding = false 
+    @passengers = []
 
   end
+
+  def speeding? 
+    @speeding
+  end
+
+  def speed 
+    @speeding = true
+  end
+
+  def add_passenger(name)
+    @passengers << name 
+  end
+
+  def num_adults
+    num_adults = 0
+
+    @passengers.each do |passenger|
+      if passenger.adult? == true
+        num_adults += 1
+      end
+    end
+    return num_adults
+  end
+
+
+
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -17,11 +17,62 @@ RSpec.describe Passenger do
 
   end
 
-  it "is a Vehicle object " do 
+  it "the vehicle is from the year 2001" do 
 
     expect(@vehicle.year).to eq("2001")
 
   end
+
+  it "its make is a Honda" do 
+
+    expect(@vehicle.make).to eq("Honda")
+
+  end
+
+  it "its model is a Civic" do 
+
+    expect(@vehicle.model).to eq("Civic")
+
+  end
+
+  it "the vehicle's default is not to speed" do 
+
+    expect(@vehicle.speeding?).to eq(false)
+
+  end
+  
+  it "the vehicle's can speed up though" do 
+
+    @vehicle.speed
+
+    expect(@vehicle.speeding?).to eq(true)
+
+  end
+
+  it "the vehicle begans with no passengers" do 
+
+    expect(@vehicle.passengers).to be_empty
+
+  end
+
+  it "the vehicle can add passengers to it" do 
+
+    @vehicle.add_passenger(@charlie) 
+    @vehicle.add_passenger(@jude)
+    @vehicle.add_passenger(@taylor)  
+
+    expect(@vehicle.passengers).to eq([@charlie, @jude, @taylor])
+
+  end
+
+  it "the vehicle can count the number of adults in it" do 
+
+    @vehicle.add_passenger(@charlie) 
+    @vehicle.add_passenger(@jude)
+    @vehicle.add_passenger(@taylor)  
+
+    expect(@vehicle.num_adults).to eq(2)
+  end 
 
 end 
 end 
