0e4a75600c045355d65bdd022755c76a80680ff2
KaraJH <karas.jones@yahoo.com>
2022-12-15 09:39:29 -0700

    feat: Park class and attributes with passing test

---
+++ lib/park.rb
@@ -0,0 +1,10 @@
+class Park 
+  attr_reader :name, :admission_price, :vehicles
+
+  def initialize(name, admission_price)
+    @name = name
+    @admission_price = admission_price
+    @vehicles = []
+  end
+
+end
