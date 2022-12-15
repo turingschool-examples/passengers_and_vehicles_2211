279f2b1df73da7e8b55ddf8135b557c62e8c91a9
Brian Hayes <Brian.hayes1200@gmail.com>
2022-12-15 09:21:59 -0700

    Feat: add driver? and drive methods with tests to class passenger

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -5,11 +5,22 @@ class Passenger
   def initialize(details)
     @name = details["name"]
     @age = details["age"]
+    @driver = false
   end
 
   def adult?
     if @age >= 18
       true
+    else
+      false
     end
+  end
+
+  def driver?
+    @driver
+  end
+
+  def drive
+    @driver = true
   end
 end
--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -3,10 +3,23 @@ require './lib/passenger'
 RSpec.describe Passenger do 
   it 'will exist and have attributes' do 
     charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})
 
     expect(charlie).to be_a(Passenger)
     expect(charlie.name).to eq("Charlie")
     expect(charlie.age).to eq(18)
     expect(charlie.adult?).to eq(true)
+    expect(taylor.adult?).to eq(false)
+  end
+
+  it 'will check to see who the driver is' do 
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+    expect(charlie.driver?).to eq(false)
+
+    charlie.drive
+
+    expect(charlie.driver?).to eq(true)
   end
 end
