a880f0cb8f14e6a8918849d42897e3a2e2f9e1a7
Jade Stewart <jadekstewart3@gmail.com>
2022-12-15 10:29:26 -0700

    Create passengers_in_park method with passing test, write test for calculating revenue

--- lib/park.rb
+++ lib/park.rb
@@ -1,15 +1,20 @@
 class Park
-    attr_reader :name,
-                :admission_price,
-                :vehicles_in_park
+	attr_reader :name,
+							:admission_price,
+							:vehicles_in_park
 
-    def initialize(name, admission)
-        @name = name
-        @admission_price = admission
-        @vehicles_in_park = []
-    end
+	def initialize(name, admission)
+		@name = name
+		@admission_price = admission
+		@vehicles_in_park = []
+	end
 
-    def add_vehicle(vehicle)
-        @vehicles_in_park << vehicle
-    end
+	def add_vehicle(vehicle)
+		@vehicles_in_park << vehicle
+	end
+
+	def passengers_in_park(vehicle)
+		vehicle.passengers.count
+	end
+	
 end
--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -3,28 +3,48 @@ require './lib/passenger'
 require './lib/park'
 
 RSpec.describe do 
-    let(:park) { Park.new("Rocky Mountain National Park", 20) }
-    let(:vehicle1) { Vehicle.new("2001", "Honda", "Civic") }
+  let(:park) { Park.new("Rocky Mountain National Park", 20) }
+  let(:vehicle1) { Vehicle.new("2001", "Honda", "Civic") }
 	let(:charlie) { Passenger.new({"name" => "Charlie", "age" => 18}) }
 	let(:taylor) { Passenger.new({"name" => "Taylor", "age" => 12}) }
 	let(:jude) { Passenger.new({"name" => "Jude", "age" => 20}) }
 
-    it "exists" do 
-        expect(park).to be_an_instance_of(Park)
-    end
+	it "exists" do 
+		expect(park).to be_an_instance_of(Park)
+	end
 
-    it "has a name" do 
-        expect(park.name).to eq("Rocky Mountain National Park")
-    end
+	it "has a name" do 
+		expect(park.name).to eq("Rocky Mountain National Park")
+	end
 
-    it "has an addmission price" do 
-        expect(park.admission_price).to eq(20)
-    end
+	it "has an addmission price" do 
+		expect(park.admission_price).to eq(20)
+	end
 
-    it "has visitors" do 
-        expect(park.vehicles_in_park).to eq([])
-        park.add_vehicle(vehicle1)
-        expect(park.vehicles_in_park).to eq([vehicle1])
-    end
-    
+	it "has visitors" do 
+		expect(park.vehicles_in_park).to eq([])
+		park.add_vehicle(vehicle1)
+		expect(park.vehicles_in_park).to eq([vehicle1])
+	end
+
+	it "can list all passengers in park" do 
+		vehicle1.add_passenger(jude)
+		vehicle1.add_passenger(charlie)
+		vehicle1.add_passenger(taylor)
+
+		park.add_vehicle(vehicle1)
+	
+		expect(park.vehicles_in_park).to eq([vehicle1])
+		expect(park.passengers_in_park(vehicle1)).to eq(3)
+	end
+
+	it "can calculate revenue" do 
+		vehicle1.add_passenger(jude)
+		vehicle1.add_passenger(charlie)
+		vehicle1.add_passenger(taylor)
+
+		park.add_vehicle(vehicle1)
+
+		expect(park.calculate_rev).to eq(40)
+	end
 end
