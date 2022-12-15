d3c22f4239e067bf00e15949e0ad0b72a7b816ba
KaraJH <karas.jones@yahoo.com>
2022-12-15 11:00:30 -0700

    test: Helper methods for patrons hash

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -184,4 +184,35 @@ RSpec.describe Park do
       expect(park.patron_list).to eq(expected)
     end
   end
+
+  describe '#visitor has helper methods' do 
+    it 'returns the passengers, adults and minors in their own arrays' do 
+      park = Park.new("Zion National Park", 15.00) 
+
+      vehicle_1 = Vehicle.new("2001", "Honda", "Civic") 
+      vehicle_2 = Vehicle.new("2005", "Chevrolet", "Aveo")  
+
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18}) 
+      jude = Passenger.new({"name" => "Jude", "age" => 20}) 
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+      cornelius = Passenger.new({"name" => "Cornelius", "age" => 50}) 
+      maud = Passenger.new({"name" => "Maud", "age" => 50}) 
+      beatrice = Passenger.new({"name" => "Beatrice", "age" => 10}) 
+
+      vehicle_1.add_passenger(charlie)  
+      vehicle_1.add_passenger(jude) 
+      vehicle_1.add_passenger(taylor) 
+
+      vehicle_2.add_passenger(cornelius)  
+      vehicle_2.add_passenger(maud) 
+      vehicle_2.add_passenger(beatrice) 
+
+      park.add_vehicle(vehicle_1)
+      park.add_vehicle(vehicle_2)
+
+      expect(park.visitor_names).to eq(["Beatrice", "Charlie", "Cornelius", "Jude", "Maud", "Taylor"])
+      expect(park.adult_visitors).to eq(["Charlie", "Cornelius", "Jude", "Maud"])
+      expect(park.minor_visitors).to eq(["Beatrice", "Taylor"])
+    end
+  end
 end
