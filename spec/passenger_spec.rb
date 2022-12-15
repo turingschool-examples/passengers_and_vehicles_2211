f98cf931c0421dcdd4effc97ffaca6b52af6a29d
KaraJH <karas.jones@yahoo.com>
2022-12-15 09:08:18 -0700

    test: adult? method test

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -13,4 +13,15 @@ RSpec.describe Passenger do
     expect(taylor.name).to eq("Taylor")
     expect(taylor.age).to eq(12)
   end
+
+  describe '#adult?' do 
+    it 'is an adult if they are 18 or older' do 
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
+      
+      expect(charlie.adult?).to eq(true)
+      expect(taylor.adult?).to eq(false)
+
+    end
+  end
 end
