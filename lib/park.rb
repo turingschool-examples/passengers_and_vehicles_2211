a2a86769069d50c8d63b9049679f193d696b7d6a
Jade Stewart <jadekstewart3@gmail.com>
2022-12-15 10:15:05 -0700

    Create add_vehicle method and pass test

--- lib/park.rb
+++ lib/park.rb
@@ -1,9 +1,15 @@
 class Park
     attr_reader :name,
-                :admission_price
-                
+                :admission_price,
+                :vehicles_in_park
+
     def initialize(name, admission)
         @name = name
         @admission_price = admission
+        @vehicles_in_park = []
+    end
+
+    def add_vehicle(vehicle)
+        @vehicles_in_park << vehicle
     end
 end
--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -4,7 +4,27 @@ require './lib/park'
 
 RSpec.describe do 
     let(:park) { Park.new("Rocky Mountain National Park", 20) }
+    let(:vehicle1) { Vehicle.new("2001", "Honda", "Civic") }
+	let(:charlie) { Passenger.new({"name" => "Charlie", "age" => 18}) }
+	let(:taylor) { Passenger.new({"name" => "Taylor", "age" => 12}) }
+	let(:jude) { Passenger.new({"name" => "Jude", "age" => 20}) }
+
     it "exists" do 
         expect(park).to be_an_instance_of(Park)
     end
+
+    it "has a name" do 
+        expect(park.name).to eq("Rocky Mountain National Park")
+    end
+
+    it "has an addmission price" do 
+        expect(park.admission_price).to eq(20)
+    end
+
+    it "has visitors" do 
+        expect(park.vehicles_in_park).to eq([])
+        park.add_vehicle(vehicle1)
+        expect(park.vehicles_in_park).to eq([vehicle1])
+    end
+    
 end
