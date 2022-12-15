65c39531ca3146aff939634d996972f45c9e49a8
Brian Hayes <Brian.hayes1200@gmail.com>
2022-12-15 09:24:59 -0700

    Test: add test for initialize method for class Vehicle

---
+++ spec/vehicle_spec.rb
@@ -0,0 +1,12 @@
+require './lib/passenger'
+require './lib/vehicle'
+
+RSpec.describe Vehicle do 
+  it 'will exist and have attributes' do 
+    vehicle = Vehicle.new("2001", "Honda", "Civic")  
+
+    expect(vehicle.year).to eq("2001")
+    expect(vehicle.make).to eq("Honda")
+    expect(vehicle.model).to eq("Civic")
+  end
+end
