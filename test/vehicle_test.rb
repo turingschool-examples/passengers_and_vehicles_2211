02ee01adf2fd793d795b366edfe3c32b79ef8f5d
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2022-12-15 10:51:12 -0700

    Create '2. #year, #make, and #model' check test

--- test/vehicle_test.rb
+++ test/vehicle_test.rb
@@ -1,13 +1,22 @@
 require './test/test_helper.rb'
 
 RSpec.describe Passenger do
-  let(:vehicle) { Vehicle.new("2001", "Honda", "Civic") } 
+  let(:vehicle) { Vehicle.new('2001', 'Honda', 'Civic') } 
   let(:taylor) { Passenger.new({'name' => 'Taylor', 'age' => 12}) }
 
   context 'Iteration 2' do
     it '1. exists' do
       expect(vehicle).to be_a(Vehicle)
     end
+    
+    it '2. can check object for year, make, and model' do
+      expect(vehicle.year).to eq('2001')
+      expect(vehicle.make).to eq('Honda')
+      expect(vehicle.model).to eq('Civic')
+    end
+
+
+
   end
 end
  
