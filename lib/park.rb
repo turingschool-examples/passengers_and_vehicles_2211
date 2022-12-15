ab48a30d2289b6fa4f1ad00f0ebab40dff00c79b
Matt Enyeart <menyeart1@gmail.com>
2022-12-15 10:50:35 -0700

    Add an initial method for generating patron lists/iteration 4

--- lib/park.rb
+++ lib/park.rb
@@ -19,6 +19,18 @@ class Park
     list_all_passengers.find_all { |passenger| passenger.adult? }.length * @admission_price
   end
 
+  def generate_patron_lists
+    patron_tracker = {}
+    patron_tracker[:adults] = list_all_passengers.find_all { |passenger| passenger.adult? }.map { |passenger| passenger.name }.sort
+    patron_tracker[:minors] = list_all_passengers.find_all { |passenger| passenger.adult? == false }.map { |passenger| passenger.name}.sort
+    patron_tracker[:all] = list_all_passengers.map { |passenger| passenger.name }.sort
+    patron_tracker
+  end
+
+
+
+
+
 
 
 
