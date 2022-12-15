dc60abf456a964b5009ce7fca878b7da0c461da7
Conner Van Loan <cdvanloan@gmail.com>
2022-12-15 09:46:33 -0700

    Adds tests for passengers in vehicle

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -19,4 +19,19 @@ RSpec.describe Vehicle do
     vehicle.speed
     expect(vehicle.speeding?).to be true
   end
+
+  it 'can add passengers to the vehcile' do
+    vehicle = Vehicle.new("2001", "Honda", "Civic")
+
+    expect(vehcile.passengers).to eq([])
+
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    jude = Passenger.new({"name" => "Jude", "age" => 20})
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+    vehicle.add_passenger(charlie)
+    vehicle.add_passenger(jude)
+    vehicle.add_passenger(taylor)
+
+    expect(vehcile.passengers).to eq(charlie, jude, taylor)
+  end
 end
