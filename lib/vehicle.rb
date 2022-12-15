c6f92b9b8eaa76d33c7a7b6a1f48ea71474d3987
Brian Hayes <Brian.hayes1200@gmail.com>
2022-12-15 09:34:57 -0700

    Feat: add passengers and add passenger method to class vehicle

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,13 +1,15 @@
 class Vehicle 
   attr_reader :year,
               :make,
-              :model
+              :model,
+              :passengers
 
   def initialize(year, make, model)
     @year = year
     @make = make 
     @model = model
     @speeding = false
+    @passengers = []
   end
 
   def speeding?
@@ -16,5 +18,9 @@ class Vehicle
 
   def speeding
     @speeding = true
+  end
+
+  def add_passenger(passenger)
+    @passengers << passenger
   end
 end
