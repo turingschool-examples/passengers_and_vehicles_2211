04bb57acbe6fd1b3a92fb5c7da747a7c01cd6f50
Matt Enyeart <menyeart1@gmail.com>
2022-12-15 09:14:21 -0700

    Add a test for the adult? method

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -17,5 +17,13 @@ it "can have an age" do
   expect(charlie.age).to eq(18)
 end
 
+it "can differentiate between being an adult or child" do
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+    expect(charlie.adult?).to eq(true)
+    expect(taylor.adult?).to eq(false)
+end
+
+
 
 end
