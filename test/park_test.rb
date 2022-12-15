682dcbc66a2430c0d3e911e67912917c6a19d299
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2022-12-15 11:59:59 -0700

    Started building test 3

--- test/park_test.rb
+++ test/park_test.rb
@@ -2,10 +2,15 @@ require './lib/park'
 
 RSpec.describe Park do
   let(:park) {Park.new('Olympic National Park', 11.50) }
-  let(:vehicle) { Vehicle.new('2001', 'Honda', 'Civic') } 
+  let(:vehicle1) { Vehicle.new('2001', 'Honda', 'Civic') }
+  let(:vehicle2) { Vehicle.new('2016', 'Subaru', 'Forester') }
+  let(:vehicle3) { Vehicle.new('2006', 'Toyota', 'Corolla') }
   let(:taylor) { Passenger.new({'name' => 'Taylor', 'age' => 12}) }
-  let(:charlie) { Passenger.new({"name" => "Charlie", "age" => 18}) }  
-  let(:jude) {Passenger.new({"name" => "Jude", "age" => 20}) }
+  let(:charlie) { Passenger.new({'name' => 'Charlie', 'age' => 18}) }  
+  let(:jude) {Passenger.new({'name' => 'Jude', 'age' => 20}) }
+  let(:isiah) { Passenger.new({'name' => 'Isiah', 'age' => 15}) }
+  let(:valorie) { Passenger.new({'name' => 'Valorie', 'age' => 25}) }  
+  let(:ulric) {Passenger.new({'name' => 'Ulric', 'age' => 10}) }
 
   context 'Iteration 3' do
     it '1. exists' do
@@ -15,6 +20,10 @@ RSpec.describe Park do
     it '2. has readable attributes' do
       expect(park.name).to eq('Olympic National Park')
       expect(park.admission).to eq(11.50)
+    end
+
+    it '3. can list all vehicles that have entered it' do
+      require 'pry'; binding.pry
     end
   end
 end
