f9d1703d0cd3a8586e0a900d53c5509ca039cf73
Meredith Trevino <meredithtrevino@gmail.com>
2022-12-15 09:59:02 -0700

    adds vehicles in park tests

--- lib/park.rb
+++ lib/park.rb
@@ -1,8 +1,13 @@
 class Park
-    attr_reader :name
+    attr_reader :name, :vehicles_in_park
 
     def initialize(name)
         @name = name
+        @vehicles_in_park = []
+    end
+
+    def enter_park(vehicle)
+        @vehicles_in_park << vehicle
     end
 
 end
--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -13,4 +13,13 @@ let(:vehicle) {Vehicle.new("2001", "Honda", "Civic")}
     it 'has a name' do
         expect(park.name).to eq("Yellowstone")
     end
+
+    it 'can list vehicles in the park' do
+        expect(park.vehicles_in_park).to eq([])
+    end
+
+    it 'can add vehicles to park' do
+        park.enter_park(vehicle)
+        expect(park.vehicles_in_park).to eq([vehicle])
+    end
 end
