37d8a6eee70da8aa2158c58b98fbaf69ceca21ca
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-12-15 08:37:02 -0800

    Test: create tests for add_passenger method

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -3,6 +3,9 @@ require './lib/vehicle'
 
 describe Vehicle do
   let (:vehicle) {Vehicle.new('2001', 'Honda', 'Civic')}
+  let(:charlie) {Passenger.new({'name' => 'Charlie', 'age' => 18})}
+  let(:taylor) {Passenger.new({'name' => 'Taylor', 'age' => 12})}
+  let(:jude) {Passenger.new({'name' => 'Jude', 'age' => 20})}
   
   it 'exists' do
     expect(vehicle).to be_a(Vehicle)
@@ -22,5 +25,17 @@ describe Vehicle do
     vehicle.speed
 
     expect(vehicle.speeding?).to be true 
+  end
+
+  it 'has no passengers by default' do
+    expect(vehicle.passengers).to eq([])
+  end
+
+  it 'can add passengers' do
+      vehicle.add_passenger(charlie)
+      vehicle.add_passenger(jude)
+      vehicle.add_passenger(taylor)
+
+      expect(vehicle.passengers).to eq([charlie, jude, taylor])
   end
 end
