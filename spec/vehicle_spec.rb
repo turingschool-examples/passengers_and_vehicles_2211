33d92509cb0df241cb909f369ddd24d362dfc325
Adam Bailey <ab67319@gmail.com>
2022-12-15 09:46:45 -0700

    Create example '2. has readable attributes', refactor initialize method and add attr_reader for instance variables

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -9,5 +9,11 @@ describe Vehicle do
     it '1. exists' do
       expect(@vehicle).to be_a(Vehicle)
     end
+    
+    it '2. has readable attributes' do
+      expect(@vehicle.year).to eq("2001")
+      expect(@vehicle.make).to eq("Honda")
+      expect(@vehicle.model).to eq("Civic")
+    end
   end
 end
