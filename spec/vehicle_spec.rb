fa2ef09d7f922b0d4b06364ac98be931c83b8480
Conner Van Loan <cdvanloan@gmail.com>
2022-12-15 09:50:45 -0700

    Adds test for num_adults

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -31,7 +31,19 @@ RSpec.describe Vehicle do
     vehicle.add_passenger(charlie)
     vehicle.add_passenger(jude)
     vehicle.add_passenger(taylor)
-    
+
     expect(vehicle.passengers).to eq([charlie, jude, taylor])
+  end
+
+  it 'can check how many adults are in the vehicle' do
+    vehicle = Vehicle.new("2001", "Honda", "Civic")
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    jude = Passenger.new({"name" => "Jude", "age" => 20})
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+    vehicle.add_passenger(charlie)
+    vehicle.add_passenger(jude)
+    vehicle.add_passenger(taylor)
+
+    expect(vehicle.num_adults).to eq(2)
   end
 end
