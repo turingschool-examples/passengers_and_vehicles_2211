9566f05cf2fb88d9ea2823cecdb4a66d9d62f0e7
David Marino <davidjmarino8@gmail.com>
2022-12-15 12:58:26 -0500

    park list vehicle complete

--- lib/park.rb
+++ lib/park.rb
@@ -1,8 +1,29 @@
 class Park
-    attr_reader :name, :admission
+    attr_reader :name, :admission, :vehicles_in_park, :revenue
 
     def initialize(name, admission)
         @name = name
         @admission = admission
+        @vehicles_in_park = []
+        @revenue = 0
     end
+
+    def add_vehicle(vehicle)
+        @vehicles_in_park << vehicle
+        
+    end
+
+    def list_passengers
+        vehicles_in_park.map do |vehicle|
+            vehicle.passengers.map do |passenger|
+                passenger
+            end
+        end.flatten
+    end
+
+    def charge
+        # require 'pry'; binding.pry
+        @revenue += @admission
+    end
+
 end
--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -14,4 +14,26 @@ RSpec.describe Vehicle do
         expect(park1.name).to eq("Jurrasic")
         expect(park1.admission).to eq(5)
     end
+
+    it 'can list all vehicles tha entered the park' do
+        park1.add_vehicle(vehicle1)
+        park1.add_vehicle(vehicle2)
+        expect(park1.vehicles_in_park).to eq([vehicle1, vehicle2])
+    end
+
+    it "can list all passengers that enter the park" do
+        park1.add_vehicle(vehicle1)
+        park1.add_vehicle(vehicle2)
+        vehicle1.add_passenger(jude)
+        vehicle1.add_passenger(taylor)
+        vehicle2.add_passenger(charlie)
+        expect(park1.list_passengers).to eq([jude, taylor, charlie])
+    end
+
+    it 'can calculate revenue' do
+        expect(park1.revenue).to eq(0)
+        park1.charge
+        park1.charge
+        expect(park1.revenue).to eq(10)
+    end
 end
