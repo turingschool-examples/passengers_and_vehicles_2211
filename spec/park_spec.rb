6aed65b4b67eb2e265402130041f426a1567ecc6
Matt Enyeart <menyeart1@gmail.com>
2022-12-15 10:51:10 -0700

    Add a test for generate patron lists method, iteration 4

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -65,6 +65,37 @@ describe Park do
     expect(park.calculate_revenue).to eq(75)  
   end
 
+  it "generate lists of attendees based on age" do
+    park = Park.new("Zion", 25)
+    honda = Vehicle.new("2001", "Honda", "Civic")
+    toyota = Vehicle.new("2004", "Toyota", "4Runner")
+    ford = Vehicle.new("2018", "Ford", "Transit")
+    charlie = Passenger.new({"name" => "Charlie", 
+    "age" => 18})
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+    sunita = Passenger.new({"name" => "Sunita", "age" => 39})
+    callie = Passenger.new({"name" => "Callie", "age" => 5})
+    stephanie = Passenger.new({"name" => "Stephanie", "age" => 33})
+    matt = Passenger.new({"name" => "Matt", "age" => 35})
+    honda.add_passenger(charlie)
+    toyota.add_passenger(taylor)
+    toyota.add_passenger(sunita)
+    toyota.add_passenger(callie)
+    ford.add_passenger(stephanie)
+    ford.add_passenger(matt)
+    park.enter_vehicle(honda)
+    park.enter_vehicle(toyota)
+    park.enter_vehicle(ford)
+    expect(park.generate_patron_lists[:adults]).to eq(["Charlie", "Matt", "Stephanie", "Sunita"])
+    expect(park.generate_patron_lists[:minors]).to eq(["Callie", "Taylor"])
+    expect(park.generate_patron_lists[:all]).to eq(["Callie", "Charlie", "Matt", "Stephanie", "Sunita", "Taylor"])
+
+  end
+
+    
+
+
+
 
 
 
