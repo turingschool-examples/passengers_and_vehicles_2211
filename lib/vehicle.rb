5763b896cdd87c7f42dd6eb80a5fcac0689c7db1
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-12-15 08:37:13 -0800

    Feat: create add_passenger method

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,11 +1,16 @@
 class Vehicle
-  attr_reader :year, :make, :model, :speeding
+  attr_reader :year, 
+              :make, 
+              :model, 
+              :speeding,
+              :passengers 
 
   def initialize(year, make, model)
     @year = year
     @make = make
     @model = model
     @speeding = false
+    @passengers = []
   end
 
   def speeding?
@@ -15,4 +20,8 @@ class Vehicle
   def speed
     @speeding = true
   end
+
+  def add_passenger(passenger)
+    @passengers.push(passenger)
+  end 
 end 
