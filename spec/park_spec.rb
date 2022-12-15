0d21b9d27334ce28d9e32f7670f74f8dd68f38bf
Weston Sandfort <sandfortw@gmail.com>
2022-12-15 10:29:01 -0700

    Add a spec for iteration 4

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -68,5 +68,49 @@ describe Park do
     expect(park.revenue).to eq(125)
   end
 
+  it 'can sort passengers alphabetically' do
+    park = Park.new("Yosemite", 25)
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    mark = Passenger.new({"name" => "Mark", "age" => 6})
+    julia = Passenger.new({"name" => "Julia", "age" => 41})
+    emily = Passenger.new({"name" => "Emily", "age" => 16})
+    jamal = Passenger.new({"name" => "Jamal", "age" => 23})
+    vehicle1 = Vehicle.new("2002", "Honda", "CRV")
+    vehicle2 = Vehicle.new("2004", "Volks", "Jetta")
+ 
+    vehicle1.add_passenger(charlie)
+    vehicle1.add_passenger(mark)
+    vehicle2.add_passenger(julia)
+    vehicle2.add_passenger(emily)
+    vehicle2.add_passenger(jamal)
+    park.add_vehicle(vehicle1)
+    park.add_vehicle(vehicle2)
+    
+    expect(park.abc_passenger_list).to eq([charlie, emily, jamal, julia, mark])
+  end
+
+  it 'can list passengers by minors and adults' do
+    park = Park.new("Yosemite", 25)
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    mark = Passenger.new({"name" => "Mark", "age" => 6})
+    julia = Passenger.new({"name" => "Julia", "age" => 41})
+    emily = Passenger.new({"name" => "Emily", "age" => 16})
+    jamal = Passenger.new({"name" => "Jamal", "age" => 23})
+    vehicle1 = Vehicle.new("2002", "Honda", "CRV")
+    vehicle2 = Vehicle.new("2004", "Volks", "Jetta")
+ 
+    vehicle1.add_passenger(charlie)
+    vehicle1.add_passenger(mark)
+    vehicle2.add_passenger(julia)
+    vehicle2.add_passenger(emily)
+    vehicle2.add_passenger(jamal)
+    park.add_vehicle(vehicle1)
+    park.add_vehicle(vehicle2)
+
+    expect(park.minors_adults).to eq({
+      :minors => [emily, mark],
+      :adults => [charlie, jamal, julia]
+    })
+  end
 
 end
