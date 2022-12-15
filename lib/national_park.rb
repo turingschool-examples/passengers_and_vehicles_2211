a24a6544472e23171c19859e4ffdd503b9481a9b
Melony Franchini <meltravelz@gmail.com>
2022-12-15 11:10:37 -0700

    feat: Add revenue_generated methodgit add .

--- lib/national_park.rb
+++ lib/national_park.rb
@@ -7,7 +7,7 @@ class NationalPark
         @name = name
         @admission_price = admission_price
         @vehicles_in_park = []
-        @revenue_generated = 0
+        @revenue = 0
     end
 
     def enter(vehicle)
@@ -22,5 +22,15 @@ class NationalPark
         all_passengers_in_park.flatten
     end
 
+    def revenue_generated
+        total_numb_adults = []
+        
+        @vehicles_in_park.each do |each_vehicle|
+            total_numb_adults << each_vehicle.num_adults
+        end
+         
+        daily_rev = total_numb_adults.sum * @admission_price 
+        @revenue = daily_rev
+    end
 
 end
