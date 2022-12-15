8b093bd2d5d1c985c102f78efaabee99a3c8ddb2
Conner Van Loan <cdvanloan@gmail.com>
2022-12-15 09:29:54 -0700

    test: adds test for IT1

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -6,8 +6,26 @@ RSpec.describe Passenger do
   it 'is an instence of Passenger and has attributes' do
     charlie = Passenger.new({"name" => "Charlie", "age" => 18})
     taylor = Passenger.new({"name" => "Taylor", "age" => 12})
-
+    
     expect(charlie.name).to eq("Charlie")
     expect(charlie.age).to eq(18)
   end
+
+  it 'can check if they are an adult' do
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+    expect(charlie.adult?).to be true
+    expect(taylor.adult?).to be false
+  end
+
+  it 'can check if they are a driver and drive' do
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+    
+    expect(charlie.driver?).to be false
+    charlie.drive
+    expect(charlie.driver?).to be true
+  end
+
 end
