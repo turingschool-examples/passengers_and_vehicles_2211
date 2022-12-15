797817e2ff8c3ba450daa21be69a32ef5afa7a88
Matt Enyeart <menyeart1@gmail.com>
2022-12-15 09:44:14 -0700

    Complete iteration 2 spec

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -23,6 +23,50 @@ it "has a model" do
   expect(vehicle.model).to eq("Civic")
 end
 
+it "does not speed by default" do
+  vehicle = Vehicle.new("2001", "Honda", "Civic")
+  expect(vehicle.speeding?).to eq(false)
+end
+
+it "it can speed" do
+  vehicle = Vehicle.new("2001", "Honda", "Civic")
+  vehicle.speed
+  expect(vehicle.speeding?).to eq(true)
+end
+
+it "it does not contain passengers by default" do
+  vehicle = Vehicle.new("2001", "Honda", "Civic")
+  expect(vehicle.passengers).to eq([])
+end
+
+it "can add passengers" do
+  vehicle = Vehicle.new("2001", "Honda", "Civic")
+  charlie = Passenger.new({"name" => "Charlie", "age" => 18})   
+  jude = Passenger.new({"name" => "Jude", "age" => 20}) 
+  taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+  vehicle.add_passenger(charlie)  
+  vehicle.add_passenger(jude) 
+  vehicle.add_passenger(taylor) 
+  expect(vehicle.passengers).to eq([charlie, jude, taylor])
+end
+
+it "can determine how many passengers are adults" do
+  vehicle = Vehicle.new("2001", "Honda", "Civic")
+  charlie = Passenger.new({"name" => "Charlie", "age" => 18})   
+  jude = Passenger.new({"name" => "Jude", "age" => 20}) 
+  taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+  vehicle.add_passenger(charlie)  
+  vehicle.add_passenger(jude) 
+  vehicle.add_passenger(taylor) 
+  expect(vehicle.num_adults).to eq(2)
+end
+
+
+
+
+
+
+
 
 
 
