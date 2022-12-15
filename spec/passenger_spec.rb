154f00803815783ae778664445e9bae35f90fad3
KaraJH <karas.jones@yahoo.com>
2022-12-15 09:05:38 -0700

    test: Passenger class with attributes

---
+++ spec/passenger_spec.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+require './lib/passenger'
+
+RSpec.describe Passenger do 
+  it 'exists and has attributes' do 
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})     
+
+    expect(charlie).to be_a(Passenger)
+    expect(taylor).to be_a(Passenger)
+    expect(charlie.name).to eq("Charlie")
+    expect(charlie.age).to eq(18)
+    expect(taylor.name).to eq("Taylor")
+    expect(taylor.age).to eq(12)
+  end
+end
