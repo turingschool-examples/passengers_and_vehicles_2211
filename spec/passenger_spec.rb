62038b100d1a7cc92ae3efe48b9245a456baa13d
Weston Sandfort <sandfortw@gmail.com>
2022-12-15 09:09:39 -0700

    Add a test for passenger

---
+++ spec/passenger_spec.rb
@@ -0,0 +1,31 @@
+require './lib/passenger'
+
+describe 'passnger' do 
+
+  it 'has a name' do
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    expect(charlie.name).to eq("Charlie")
+
+  end
+
+  it 'has an age' do
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    expect(charlie.age).to eq(18)
+  end
+
+  it 'can be an adult' do
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
+
+    expect(charlie.adult?).to be(true)
+    expeect(taylor.adult?).to be(false)
+  end
+
+  it 'can be a driver' do
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    expect(charlie.driver?).to be(false)
+    charlie.driver
+    expect(charlie.driver?).to be(true)
+  end
+
+end
