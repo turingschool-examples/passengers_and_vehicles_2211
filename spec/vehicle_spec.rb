57d6fec940be8acb8c3dbeb3bad855c8a0c146ed
KaraJH <karas.jones@yahoo.com>
2022-12-15 09:24:23 -0700

    test: Add passenger method

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe Vehicle do
     expect(vehicle.year).to eq("2001")
     expect(vehicle.make).to eq("Honda")
     expect(vehicle.model).to eq("Civic")
-    expect(vehicle.is_speeding).to eq(false)
     expect(vehicle.passengers).to eq([])
   end
 
@@ -21,6 +20,24 @@ RSpec.describe Vehicle do
       vehicle.speed
 
       expect(vehicle.speeding?).to eq(true)
+    end
+  end
+
+  describe '#add_passenger' do 
+    it 'can add passengers to the vehicle' do 
+      vehicle = Vehicle.new("2001", "Honda", "Civic") 
+
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18}) 
+      jude = Passenger.new({"name" => "Jude", "age" => 20}) 
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+      expect(vehicle.passengers).to eq([])
+
+      vehicle.add_passenger(charlie)  
+      vehicle.add_passenger(jude) 
+      vehicle.add_passenger(taylor) 
+
+      expect(vehicle.passengers).to eq([charlie, jude, taylor])
     end
   end
 end
