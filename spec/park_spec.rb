f3babe19b0f3592ede0f87950b2ebf00d085f3e6
KaraJH <karas.jones@yahoo.com>
2022-12-15 09:42:43 -0700

    test: Add vehicle method test

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -11,4 +11,20 @@ RSpec.describe Park do
     expect(park.admission_price).to eq(15.00)
     expect(park.vehicles).to eq([])
   end
+
+  describe '#add_vehicle' do 
+    it 'can add vehicles to the park' do 
+      park = Park.new("Zion National Park", 15.00) 
+
+      vehicle_1 = Vehicle.new("2001", "Honda", "Civic") 
+      vehicle_2 = Vehicle.new("2005", "Chevrolet", "Aveo") 
+
+      expect(park.vehicles).to eq([])
+
+      park.add_vehicle(vehicle_1)
+      park.add_vehicle(vehicle_2)
+
+      expect(park.vehicles).to eq([vehicle_1, vehicle_2])
+    end
+  end
 end
