2fd248fdb1d0315f4a9b8de0effeb43f4546c193
Meredith Trevino <meredithtrevino@gmail.com>
2022-12-15 09:29:24 -0700

    adds vehicle class and 4 working tests

---
+++ spec/vehicle_spec.rb
@@ -0,0 +1,23 @@
+require './lib/passenger'
+require './lib/vehicle'
+
+RSpec.describe Vehicle do
+    let(:vehicle) {Vehicle.new("2001", "Honda", "Civic")} 
+    
+    it 'exists' do
+        expect(vehicle).to be_an_instance_of(Vehicle)
+    end
+
+    it 'has a year' do
+        expect(vehicle.year).to eq("2001")
+    end
+
+    it 'has a make' do
+        expect(vehicle.make).to eq("Honda")
+    end
+
+    it 'has a model' do
+        expect(vehicle.model).to eq("Civic")
+    end
+
+end
