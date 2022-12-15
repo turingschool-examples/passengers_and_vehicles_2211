9f43909b5f134c71fbcbe9ffb2c3eea29354ff50
James Taylor <jrtaylor0614@gmail.com>
2022-12-15 11:25:14 -0700

    Feat: Vehicle class, exists, has attributes

---
+++ spec/vehicle_spec.rb
@@ -0,0 +1,18 @@
+require './lib/vehicle'
+require './lib/passenger'
+
+RSpec.describe Vehicle do
+    before(:each) do
+        @vehicle = Vehicle.new("2001", "Honda", "Civic")
+    end
+
+    it "exists" do 
+        expect(@vehicle).to be_an_instance_of(Vehicle)
+    end
+
+    it "has attributes" do
+        expect(@vehicle.year).to eq("2001")
+        expect(@vehicle.make).to eq("Honda")
+        expect(@vehicle.model).to eq("Civic")
+    end
+end
