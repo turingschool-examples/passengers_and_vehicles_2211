1c9133dd537d2b66e3c82f1d977ada52767b0460
Meredith Trevino <meredithtrevino@gmail.com>
2022-12-15 09:16:59 -0700

    adds working test for adult?

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,8 +1,14 @@
 class Passenger
-    attr_reader :name, :age
+    attr_reader :name, :age, :adult
 
     def initialize(info)
         @name = info["name"]
         @age = info["age"]
+        @adult = adult
+    end
+
+    def adult?
+        return true if age >= 18
+        false
     end
 end
--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -15,4 +15,12 @@ RSpec.describe Passenger do
         expect(charlie.name).to eq("Charlie")
         expect(charlie.age).to eq(18)
     end
+
+    it 'can test if an adult' do
+        charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+        taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+        expect(charlie.adult?).to be(true)
+        expect(taylor.adult?).to be(false)
+    end
 end
