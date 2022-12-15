0dde54890da579e69092ec3210e4ad91284be831
Amy Spears <amspears007@gmail.com>
2022-12-15 10:26:49 -0700

    Add passing speed tests in Vehicle class

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -7,4 +7,25 @@ RSpec.describe Vehicle do
 
     expect(vehicle).to be_an_instance_of(Vehicle)
   end
+
+  it 'has attributes' do
+    vehicle = Vehicle.new("2001", "Honda", "Civic")    
+
+    expect(vehicle.year).to eq("2001")
+    expect(vehicle.make).to eq("Honda")
+    expect(vehicle.model).to eq("Civic")
+  end
+
+  it 'starts not speeding' do
+  vehicle = Vehicle.new("2001", "Honda", "Civic")    
+
+    expect(vehicle.speeding?).to eq(false)
+  end
+
+  it 'can speed' do
+    vehicle = Vehicle.new("2001", "Honda", "Civic")    
+    vehicle.speed
+
+    expect(vehicle.speeding?).to eq(true)
+  end
 end
