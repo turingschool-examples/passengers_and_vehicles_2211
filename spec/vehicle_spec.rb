1b68e912f31d95136da67daccb5cf57fe400107b
Brian Hayes <Brian.hayes1200@gmail.com>
2022-12-15 09:33:23 -0700

    Test: add test for passengers and add passengers method for class Vehicle

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -19,4 +19,20 @@ RSpec.describe Vehicle do
 
     expect(vehicle.speeding?).to eq(true)
   end
+
+  it 'will start of with no passengers byt can add passengers' do 
+    vehicle = Vehicle.new("2001", "Honda", "Civic")  
+
+    expect(vehicle.passengers).to eq([])
+
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})  
+    jude = Passenger.new({"name" => "Jude", "age" => 20}) 
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
+
+    vehicle.add_passenger(charlie)
+    vehicle.add_passenger(jude)    
+    vehicle.add_passenger(taylor)  
+
+    expect(vehicle.passengers).to eq([charlie, jude, taylor])
+  end
 end
