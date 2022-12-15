2096ec09223c67b1942005e8028e41b0a205d758
Melony Franchini <meltravelz@gmail.com>
2022-12-15 09:35:24 -0700

    test: Set up spec file for use

---
+++ spec/vehicle_spec.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+require './lib/passenger'
+require './lib/vehicle'
+
+RSpec.describe Vehicle do
+    let(:vehicle) { Vehicle.new("2001", "Honda", "Civic") }
+
+    it "exists" do
+        
+    end
+
+
+end
