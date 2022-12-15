cce20b52af310ef59b17c0960ba9e850eeffd898
Conner Van Loan <cdvanloan@gmail.com>
2022-12-15 10:22:31 -0700

    Adds tests for visitors

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -20,4 +20,18 @@ RSpec.describe Park do
     canyonlands.admit_vehicle(vehicle)
     expect(canyonlands.vehciles_entered).to eq([vehicle])
   end
+
+  it 'can list the passengers that have entered in vehicles' do
+    canyonlands = Park.new('Canyonlands', 20)
+    vehicle = Vehicle.new("2001", "Honda", "Civic")
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    jude = Passenger.new({"name" => "Jude", "age" => 20})
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+    vehicle.add_passenger(charlie)
+    vehicle.add_passenger(jude)
+    vehicle.add_passenger(taylor)
+    canyonlands.admit_vehicle(vehicle)
+
+    expect(canyonlands.visitors).to eq([charlie, june, taylor])
+  end
 end
