1b336fb0e4a65435d986217a242c85400a4d6c12
Meredith Trevino <meredithtrevino@gmail.com>
2022-12-15 10:37:39 -0700

    working it3 tests

--- lib/park.rb
+++ lib/park.rb
@@ -1,13 +1,33 @@
 class Park
-    attr_reader :name, :vehicles_in_park
+    attr_reader :name, 
+                :vehicles_in_park, 
+                :passengers_in_park, 
+                :revenue,
+                :entry_fee
 
     def initialize(name)
         @name = name
         @vehicles_in_park = []
+        @passengers_in_park = []
+        @revenue = 0
+        @entry_fee = 15
     end
 
     def enter_park(vehicle)
         @vehicles_in_park << vehicle
+    end
+
+    def passenger_enter(passenger)
+        @passengers_in_park << passenger
+    end
+
+    def entry_fee
+        @revenue += 15
+    end
+
+    def charge_fee
+        currently_in_park = @passengers_in_park.count
+        currently_in_park * entry_fee
     end
 
 end
--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -5,6 +5,8 @@ require './lib/park'
 RSpec.describe Park do
 let(:park) {Park.new("Yellowstone")}
 let(:vehicle) {Vehicle.new("2001", "Honda", "Civic")} 
+let(:jude) {Passenger.new({"name" => "Jude", "age" => 20})}
+let(:taylor) {Passenger.new({"name" => "Taylor", "age" => 12})}
 
     it 'exists' do
         expect(park).to be_an_instance_of(Park)
@@ -21,5 +23,33 @@ let(:vehicle) {Vehicle.new("2001", "Honda", "Civic")}
     it 'can add vehicles to park' do
         park.enter_park(vehicle)
         expect(park.vehicles_in_park).to eq([vehicle])
+    end
+
+    it 'can list passengers that enter park' do
+        expect(park.passengers_in_park).to eq([])
+    end
+
+    it 'can add passengers to park' do
+        park.passenger_enter(jude)
+
+        expect(park.passengers_in_park).to eq([jude])
+    end
+
+    it 'can calculate revenue generated' do
+        expect(park.revenue).to eq(0)
+    end
+
+    it 'can charge per adult' do
+        park.enter_park(vehicle)
+        park.passenger_enter(jude)
+
+        park.charge_fee
+
+        expect(park.revenue).to eq(15)
+
+        park.passenger_enter(taylor)
+        park.charge_fee
+        expect(park.revenue).to eq(30)
+
     end
 end
