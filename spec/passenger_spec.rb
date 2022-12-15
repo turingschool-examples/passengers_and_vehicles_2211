9a58ec05d272268f865cb91319959b401c956ce6
Matt Enyeart <menyeart1@gmail.com>
2022-12-15 09:10:51 -0700

    Create a test for passenger name

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -3,12 +3,13 @@ require './lib/passenger.rb'
 describe Passenger do
 
 it "exsits" do
-  passenger = Passenger.new()  
+  charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+  expect(charlie).to be_an_instance_of(Passenger)
 end
 
 it "can have a name" do
   charlie = Passenger.new({"name" => "Charlie", "age" => 18})
-  expect(charlie.name).to eq("Chalie")
+  expect(charlie.name).to eq("Charlie")
 end
 
 end
