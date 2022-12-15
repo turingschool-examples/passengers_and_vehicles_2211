f08f4fe1ad6d0d48f745d3fc158a7b5901981ce9
Brian Hayes <Brian.hayes1200@gmail.com>
2022-12-15 10:05:00 -0700

    Feat: add vehicles and passengers methods for class NationalPark

--- lib/national_park.rb
+++ lib/national_park.rb
@@ -1,9 +1,23 @@
 class NationalPark
   attr_reader :name,
-              :admission_cost
+              :admission_cost,
+              :vehicles
   
   def initialize(name, cost)
     @name = name
     @admission_cost = cost
+    @vehicles = []
+  end
+
+  def add_vehicle(vehicle)
+    @vehicles << vehicle
+  end
+
+  def passengers
+    passengers = []
+    @vehicles.each do |vehicle|
+      passengers << vehicle.passengers 
+    end
+    passengers.flatten
   end
 end
